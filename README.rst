444ceab42ef960ab1ba9ce0a6c09de6ffc6df850
Will Vousden <will@vousden.me.uk>
2014-06-26 15:03:14 +0100

    Updated metadata.

---
+++ README.rst
@@ -0,0 +1,4 @@
+mpltex
+=====
+
+Some tools for producing *matplotlib* figures that sit will in a LaTeX document.
--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python
 
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
 except ImportError:
-    from distutils.core import setup
+    from distutils.core import setup, find_packages
+
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
         name='mpltex',
         version='0.1.0',
         author='Will Vousden',
         author_email='will@vousden.me.uk',
-        packages=['mpltex'],
+        license='MIT',
+        keywords=['latex', 'tex', 'mpl', 'matplotlib', 'plotting', 'plot'],
+        packages=find_packages(exclude=['docs', 'tests*']),
         url='https://github.com/willvousden/mpltex',
         description='Utility for plotting for LaTeX from Matplotlib.',
         license='',
