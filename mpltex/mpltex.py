5d544d3775a82faaf294a868cdc2fc8e153689f1
Will Vousden <will@vousden.me.uk>
2014-10-06 14:42:08 +0100

    Added presets.

--- mpltex/mpltex.py
+++ mpltex/mpltex.py
@@ -1,4 +1,4 @@
-__all__ = ['getDpi', 'getContext']
+__all__ = ['getDpi', 'getContext', 'getPreset']
 
 import matplotlib as mpl
 from math import sqrt
@@ -6,12 +6,29 @@ from math import sqrt
 def getDpi():
     return 72.27
 
-def getContext(columnWidth, fontSize, aspectRatio=None, height=None):
+_goldenRatio = (1 + sqrt(5)) / 2
+_presets = {
+    'revtex12-single': (468, 10.95, None), # Single-column revtex; 12pt.
+    'mnras': (240, 8, _goldenRatio * 0.8), # Double-column MNRAS; default font size.
+    'mnras-2': (504, 8, _goldenRatio) # Single-column MNRAS; default font size.
+}
+
+def getPreset(preset):
+    return _presets[preset]
+
+def getContext(preset=None, columnWidth=None, fontSize=None, aspectRatio=None, height=None):
+    if preset is not None:
+        assert preset in _presets, \
+            'Preset not found.'
+        columnWidth, fontSize, aspectRatio = _presets[preset]
+    assert columnWidth is not None and fontSize is not None, \
+        'Column width or font size missing.'
+
     dpi = getDpi() # One inch in points (according to TeX).
     width = columnWidth / dpi
     if height is None:
         if aspectRatio is None:
-            aspectRatio = (1 + sqrt(5)) / 2
+            aspectRatio = _goldenRatio
         height = width / aspectRatio
 
     rc = {}
